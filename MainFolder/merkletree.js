2fb461392ea05343ef7b166ffa35cdf0c5accb5b
Vaibhav Gupta <96335421+vaiYodo1@users.noreply.github.com>
2022-03-23 19:17:07 +0900

    Add files via upload

---
+++ MainFolder/merkletree.js
@@ -0,0 +1,41 @@
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+
+// 2. Collect list of wallet addresses from competition, raffle, etc.
+// Store list of addresses in some data sheeet (Google Sheets or Excel)
+let whitelistAddresses = [
+    "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db",
+    "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2" // The address in remix
+  ];
+
+// 3. Create a new array of `leafNodes` by hashing all indexes of the `whitelistAddresses`
+// using `keccak256`. Then creates a Merkle Tree object using keccak256 as the algorithm.
+//
+// The leaves, merkleTree, and rootHas are all PRE-DETERMINED prior to whitelist claim
+const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true});
+
+// 4. Get root hash of the `merkleeTree` in hexadecimal format (0x)
+// Print out the Entire Merkle Tree.
+const rootHash = merkleTree.getRoot();
+console.log('Whitelist Merkle Tree\n', merkleTree.toString());
+console.log("Root Hash: ", rootHash);
+
+// ***** ***** ***** ***** ***** ***** ***** ***** // 
+
+// CLIENT-SIDE: Use `msg.sender` address to query and API that returns the merkle proof
+// required to derive the root hash of the Merkle Tree
+
+// ✅ Positive verification of address
+const claimingAddress = leafNodes[0];
+// ❌ Change this address to get a `false` verification
+// const claimingAddress = keccak256("0X5B38DA6A701C568545DCFCB03FCB875F56BEDDD6");
+
+// `getHexProof` returns the neighbour leaf and all parent nodes hashes that will
+// be required to derive the Merkle Trees root hash.
+const hexProof = merkleTree.getHexProof(claimingAddress);
+console.log(hexProof);
+
+// ✅ - ❌: Verify is claiming address is in the merkle tree or not.
+// This would be implemented in your Solidity Smart Contract
+console.log(merkleTree.verify(hexProof, claimingAddress, rootHash));
