3af489b2bc92d591099b4491c447ed7ed010702b
Vaibhav Gupta <96335421+vaiYodo1@users.noreply.github.com>
2022-03-23 19:16:17 +0900

    Update YakuzaKumiai.sol

--- MainFolder/contracts/YakuzaKumiai.sol
+++ MainFolder/contracts/YakuzaKumiai.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract YakuzaKumiai is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -24,4 +25,26 @@ contract YakuzaKumiai is ERC721URIStorage, Ownable {
 
         return newItemId;
     }
+
+    // --- PROPERTIES ---- //
+
+    // Calculated from `merkletree.js`
+    bytes32 public merkleRoot =
+        0x39a01635c6a38f8beb0adde454f205fffbb2157797bf1980f8f93a5f70c9f8e6;
+
+    mapping(address => bool) public whitelistClaimed;
+
+    // --- FUNCTIONS ---- //
+
+    function whitelistMint(bytes32[] calldata _merkleProof) public {
+        require(!whitelistClaimed[msg.sender], "Address already claimed");
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+            "Invalid Merkle Proof."
+        );
+        whitelistClaimed[msg.sender] = true;
+    }
 }
+
+
